653d86f16409cf3580cb7cbb74003da065ff14b1
xbmchina <1131663064@163.com>
2018-07-09 20:10:50 +0800

    first commit

---
+++ src/common/fade/Fade.vue
@@ -0,0 +1,19 @@
+<template>
+      <transition>
+            <slot></slot>
+      </transition>
+</template>
+
+<script>
+  export default {
+      name: 'Fade'
+     
+  }
+</script>
+
+<style lang="stylus" scoped>
+.v-enter,.v-leave-to
+      opactity: 0
+.v-enter-active, .v-leave-active
+      transition: opactity .5%
+</style>
---
+++ src/page/home/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="header">
+<div class="header-left">
+	<div class="inconfont back-icon">&#xe601;</div>
+</div>
+<div class="header-input">
+	<span class="inconfont">&#xe61a;</span>
+	输入城市/景点/游玩主题
+</div>
+	<router-link to="/city">
+		<div class="header-right">
+			{{this.currentCity}}
+			<span class="inconfont arrow_icon">&#xe64a;</span>
+		</div>
+	</router-link>
+</div>
+</template>
+
+<script>
+import {  mapState } from 'vuex'
+export default {
+  name: 'HomeHeader',
+  computed: {
+      	...mapState({
+      		currentCity: 'city'
+      	}) 
+      }
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+.header
+		display:flex
+		line-height: $headerHeight
+		background:$bgColor
+		color:#fff
+		.header-left
+			width: .64rem
+			float:left
+			.back-icon
+				text-align:center
+		.header-input
+			flex: 1
+			height:.64rem
+			line-height: .64rem
+			padding-left:.2rem
+			margin-top: .12rem
+			margin-left: .2rem
+			background:#fff
+			border-radius: .1rem
+			color:#ccc
+		.header-right
+			min-width:1.04rem
+			padding: 0 .1rem
+			float:right
+			color: #fff
+			text-align:center
+			.arrow_icon
+				margin-left: -.04rem
+				font-size: .24rem
+
+
+
+</style>
